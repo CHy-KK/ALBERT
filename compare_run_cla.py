efda1985e9d0a324adf4e8cd7ac578f6348fb2be
CHy-KK <45690293+CHy-KK@users.noreply.github.com>
2020-01-12 11:58:19 +0800

    Update and rename create_run_cla.py to compare_run_cla.py

--- compare_run_cla.py
+++ compare_run_cla.py
@@ -559,21 +559,21 @@ class AXProcessor(DataProcessor):
 		return examples
 class SelfProcessor(DataProcessor):
 	def get_train_examples(self, data_dir):
-		file_path = os.path.join(data_dir, 'Lite_train.csv')
+		file_path = os.path.join(data_dir, 'train_5neg.csv')
 		df_train = pd.read_csv(file_path)
 		examples = []
 		index = -1
 		for _ , row in df_train.iterrows():
 			index += 1
 			guid = 'train-%d' % index
-			text_a = self.process_text(str(row[5]))
+			text_a = self.process_text(str(row[5])).lower()
 			abstract = str(row[3])
 			title = str(row[4])
 			if abstract != 'NO_CONTENT' and abstract != 'no_content' and abstract != 'nan':
 				content = title + abstract
 			else:
 				content = title
-			text_b = self.process_text(content)
+			text_b = self.process_text(content).lower()
 			label = str(row[2])
 			examples.append(InputExample(guid=guid, text_a=text_a,
 										text_b=text_b, label=label))
@@ -661,14 +661,14 @@ class SelfProcessor(DataProcessor):
 		for _ , row in df_test.iterrows():
 			index_des += 1
 			guid = 'test-%d' % index_des
-			text_a = self.process_text(str(row[4]))
+			text_a = self.process_text(str(row[4])).lower()
 			abstract = str(row[2])
 			title = str(row[3])
 			if abstract != 'NO_CONTENT' and abstract != 'no_content' and abstract != 'nan':
 				content = title + abstract
 			else:
 				content = title
-			text_b = self.process_text(content)
+			text_b = self.process_text(content).lower()
 			#examples.append([text_a,text_b])
 			label = "0"
 			examples.append(InputExample(guid=guid, text_a=text_a,
@@ -696,8 +696,7 @@ class SelfProcessor(DataProcessor):
 	def get_labels(self):
 		return ["0", "1"]
 
-def convert_single_example(ex_index, example, label_list, max_seq_length,
-													 tokenizer, task_name):
+def convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, task_name, tokens_stride=64):
 	"""Converts a single `InputExample` into a single `InputFeatures`."""
 
 	if isinstance(example, PaddingInputExample):
@@ -715,14 +714,40 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
 
 	tokens_a = tokenizer.tokenize(example.text_a)
 	tokens_b = None
+	tokens_a_list = []
+	tokens_b_list = []
 	if example.text_b:
 		tokens_b = tokenizer.tokenize(example.text_b)
-
+	else:
+		tokens_b = None
+	# account for [CLS] [SEP] [SEP] with -3
+	max_tokens_length = ((max_seq_length) - 3) / 2
+	
 	if tokens_b:
 		# Modifies `tokens_a` and `tokens_b` in place so that the total
 		# length is less than the specified length.
 		# Account for [CLS], [SEP], [SEP] with "- 3"
-		_truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+		# _truncate_seq_pair(tokens_a,: tokens_b, max_seq_length - 3)
+         	start_offset = 0
+		while start_offset < len(tokens_a):
+			length = len(tokens_a) - start_offset
+			if length > max_tokens_length:
+				length = max_tokens_length
+			tokens_a_list.append(tokens_a[start_offset:start_offset + length])
+			if start_offset + length == len(tokens_a)
+				break
+			start_offset += tokens_stride
+		
+         	start_offset = 0
+		while start_offset < len(tokens_b):
+			length = len(tokens_b) - start_offset
+			if length > max_tokens_length:
+				length = max_tokens_length
+			tokens_b_list.append(tokens_b[start_offset:start_offset + length])
+			if start_offset + length == len(tokens_b)
+				break
+			start_offset += tokens_stride
+				
 	else:
 		# Account for [CLS] and [SEP] with "- 2"
 		if len(tokens_a) > max_seq_length - 2:
@@ -745,76 +770,87 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
 	#
 	# For classification tasks, the first vector (corresponding to [CLS]) is
 	# used as the "sentence vector". Note that this only makes sense because
-	# the entire model is fine-tuned.
-	tokens = []
-	segment_ids = []
-	tokens.append("[CLS]")
-	segment_ids.append(0)
-	for token in tokens_a:
-		tokens.append(token)
-		segment_ids.append(0)
-	tokens.append("[SEP]")
-	segment_ids.append(0)
+	# the entire model is fine-tuned
+	feature_list = []
+	
+	for tokens_a in tokens_a_list:
+		for tokens_b in tokens_b_list:
+			tokens = []
+			segment_ids = []
+			tokens.append("[CLS]")
+			segment_ids.append(0)
+			for token in tokens_a:
+				tokens.append(token)
+				segment_ids.append(0)
+			tokens.append("[SEP]")
+			segment_ids.append(0)
 
-	if tokens_b:
-		for token in tokens_b:
-			tokens.append(token)
-			segment_ids.append(1)
-		tokens.append("[SEP]")
-		segment_ids.append(1)
+			for token in tokens_b:
+				tokens,append(token)
+				segment_ids.append(1)
+			tokens.append("[SEP]")
+			segment_ids.append(1)			
+			if tokens_b:
+				for token in tokens_b:
+					tokens.append(token)
+					segment_ids.append(1)
+				tokens.append("[SEP]")
+				segment_ids.append(1)
 
-	input_ids = tokenizer.convert_tokens_to_ids(tokens)
+			input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-	# The mask has 1 for real tokens and 0 for padding tokens. Only real
-	# tokens are attended to.
-	input_mask = [1] * len(input_ids)
+			# The mask has 1 for real tokens and 0 for padding tokens. Only real
+			# tokens are attended to.
+			input_mask = [1] * len(input_ids)
 
-	# Zero-pad up to the sequence length.
-	while len(input_ids) < max_seq_length:
-		input_ids.append(0)
-		input_mask.append(0)
-		segment_ids.append(0)
+			# Zero-pad up to the sequence length.
+			while len(input_ids) < max_seq_length:
+				input_ids.append(0)
+				input_mask.append(0)
+				segment_ids.append(0)
 
-	assert len(input_ids) == max_seq_length
-	assert len(input_mask) == max_seq_length
-	assert len(segment_ids) == max_seq_length
+			assert len(input_ids) == max_seq_length
+			assert len(input_mask) == max_seq_length
+			assert len(segment_ids) == max_seq_length
 
-	if task_name != "sts-b":
-		label_id = label_map[example.label]
-	else:
-		label_id = example.label
-	'''
-	if ex_index < 5:
-		tf.logging.info("*** Example ***")
-		tf.logging.info("guid: %s" % (example.guid))
-		tf.logging.info("tokens: %s" % " ".join(
-				[tokenization.printable_text(x) for x in tokens]))
-		tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-		tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-		tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-		tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
-	'''
-	feature = InputFeatures(
-			input_ids=input_ids,
-			input_mask=input_mask,
-			segment_ids=segment_ids,
-			label_id=label_id,
-			is_real_example=True)
-	return feature
+			if task_name != "sts-b":
+				label_id = label_map[example.label]
+			else:
+				label_id = example.label
+			'''
+			if ex_index < 5:
+				tf.logging.info("*** Example ***")
+				tf.logging.info("guid: %s" % (example.guid))
+				tf.logging.info("tokens: %s" % " ".join(
+						[tokenization.printable_text(x) for x in tokens]))
+				tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+				tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+				tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+				tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
+			'''
+			feature = InputFeatures(
+					input_ids=input_ids,
+					input_mask=input_mask,
+					segment_ids=segment_ids,
+					label_id=label_id,
+					is_real_example=True)
+			feature_list.append(feature)
+	return feature_list
 
 
 def file_based_convert_examples_to_features(
-		examples, label_list, max_seq_length, tokenizer, output_file, task_name):
+		examples, label_list, max_seq_length, tokenizer, output_file, task_name, tokens_stride):
 	"""Convert a set of `InputExample`s to a TFRecord file."""
 
 	writer = tf.python_io.TFRecordWriter(output_file)
-
+	batch_index = []
 	for (ex_index, example) in enumerate(examples):
 		if ex_index % 10000 == 0:
 			tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
 
-		feature = convert_single_example(ex_index, example, label_list,
-																		 max_seq_length, tokenizer, task_name)
+		feature_list = convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, task_name, tokens_stride)
+
+		batch_index.extend([ex_index]*len(feature_list)) 
 
 		def create_int_feature(values):
 			f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
@@ -823,19 +859,20 @@ def file_based_convert_examples_to_features(
 		def create_float_feature(values):
 			f = tf.train.Feature(float_list=tf.train.FloatList(value=list(values)))
 			return f
+		for feature in feature_list:
+			features = collections.OrderedDict()
+			features["input_ids"] = create_int_feature(feature.input_ids)
+			features["input_mask"] = create_int_feature(feature.input_mask)
+			features["segment_ids"] = create_int_feature(feature.segment_ids)
+			features["label_ids"] = create_float_feature([feature.label_id])\
+					if task_name == "sts-b" else create_int_feature([feature.label_id])
+			features["is_real_example"] = create_int_feature(
+					[int(feature.is_real_example)])
 
-		features = collections.OrderedDict()
-		features["input_ids"] = create_int_feature(feature.input_ids)
-		features["input_mask"] = create_int_feature(feature.input_mask)
-		features["segment_ids"] = create_int_feature(feature.segment_ids)
-		features["label_ids"] = create_float_feature([feature.label_id])\
-				if task_name == "sts-b" else create_int_feature([feature.label_id])
-		features["is_real_example"] = create_int_feature(
-				[int(feature.is_real_example)])
-
-		tf_example = tf.train.Example(features=tf.train.Features(feature=features))
-		writer.write(tf_example.SerializeToString())
+			tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+			writer.write(tf_example.SerializeToString())
 	writer.close()
+	return batch_index
 
 
 def file_based_input_fn_builder(input_file, seq_length, is_training,
